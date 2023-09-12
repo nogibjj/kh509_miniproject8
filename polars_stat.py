cc6cd93b9f74229e0daebf16c84051b24c0bac23
katelyn-hucker <143521756+katelyn-hucker@users.noreply.github.com>
2023-09-12 21:57:52 +0000

    update the stat script

--- polars_stat.py
+++ polars_stat.py
@@ -4,15 +4,16 @@ import matplotlib.pyplot as plt
 df1 = pl.read_csv('iris.csv')
 
 def calc_desc_stat(dataset_col):
-    out=dataset_col.describe()
-    return round(out,2)
+    out = dataset_col.describe()
+    return out
 
 print(calc_desc_stat(df1['petal.length']))
 #print(df1.columns)
 def boxplot_of_col(col):
-    plt.boxplot(col)
+    plt.boxplot(col.to_list())
+    plt.xlabel(col.name)
     plt.show()
     plt.savefig("boxplot.png")
 
 
-boxplot_of_col(df1,'petal.length')
+boxplot_of_col(df1['petal.length'])
