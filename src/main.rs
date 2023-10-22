53ffb2c46cff74104892d34e162f7e481a8e0208
katelyn-hucker <143521756+katelyn-hucker@users.noreply.github.com>
2023-10-22 15:43:13 +0000

    get times

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use csv::ReaderBuilder;
 use serde::Deserialize;
 use std::error::Error;
+use std::time::Instant;
+use std::mem;
+use libc::{self, RUSAGE_SELF, timeval, rusage};
 
 #[derive(Debug, Deserialize)]
 pub struct IrisData {
@@ -42,8 +45,21 @@ fn calculate_summary_stats(data: &[IrisData]) -> (f64, f64, f64, f64) {
 
     (mean, std_dev, min_value, max_value)
 }
+fn get_resource_usage() -> rusage {
+    let mut usage: rusage = unsafe { mem::zeroed() };
+    let _ = unsafe { libc::getrusage(RUSAGE_SELF, &mut usage) };
+    usage
+}
+
+fn timeval_to_seconds(tv: timeval) -> f64 {
+    tv.tv_sec as f64 + (tv.tv_usec as f64 / 1_000_000.0)
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let start_time = Instant::now();
+
+    let usage_start = get_resource_usage();
+
     let data = read_iris_data("iris.csv")?;
     let (mean, std_dev, min_value, max_value) = calculate_summary_stats(&data);
 
@@ -52,5 +68,18 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("Minimum Sepal Length: {:.2}", min_value);
     println!("Maximum Sepal Length: {:.2}", max_value);
 
+    let end_time = Instant::now();
+    let execution_time = end_time.duration_since(start_time);
+
+    let usage_end = get_resource_usage();
+
+    let user_cpu_time = timeval_to_seconds(usage_end.ru_utime) - timeval_to_seconds(usage_start.ru_utime);
+    let system_cpu_time = timeval_to_seconds(usage_end.ru_stime) - timeval_to_seconds(usage_start.ru_stime);
+
+    println!("User CPU time: {:.6} seconds", user_cpu_time);
+    println!("System CPU time: {:.6} seconds", system_cpu_time);
+
+    println!("Execution time: {:?}", execution_time);
     Ok(())
 }
+
